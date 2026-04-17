242bd10241907118e83681b464db49b2181bf270
Marcimave <marcimavegithub@gmail.com>
2026-04-17 11:44:31 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 /*alert("Déterminé à sortir de la précarité");*/
+function choisirOffre(nom, prix) {
+  alert("Vous avez choisi : " + nom + " - " + prix + "€");
+
+  document.querySelectorAll(".carte").forEach(c => c.classList.remove("active"));
+
+  event.target.closest(".carte").classList.add("active");
+}
