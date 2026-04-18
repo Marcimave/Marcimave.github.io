e4f8229a1c5692216797cc9d6cb3bb167159224d
Marcimave <marcimavegithub@gmail.com>
2026-04-18 16:34:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-// Initialiser EmailJS
+// Initialisation EmailJS
 emailjs.init("TON_PUBLIC_KEY");
 
 document.getElementById("formulaire").addEventListener("submit", function(e) {
   e.preventDefault();
 
   emailjs.sendForm(
-    "service_t379mfh",     // ton Service ID (OK)
-    "TON_TEMPLATE_ID",     // ⚠️ à remplacer
-    this                   // formulaire
+    "service_t379mfh",
+    "template_kwa4moh", // 👈 TON TEMPLATE
+    this
   )
   .then(function() {
     document.getElementById("confirmation").innerText =
       "Message envoyé avec succès ✅";
-  }, function(error) {
+  })
+  .catch(function(error) {
     document.getElementById("confirmation").innerText =
       "Erreur lors de l’envoi ❌";
     console.log(error);
